59fc79473f03c6d752cf4ae0eb673fb23c3e1df8
Lê Nhật Minh <lenhatminh11a1@gmail.com>
2022-01-23 00:01:28 +0700

    using react spring to animate number

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,42 +1,50 @@
-import {FaUser} from "react-icons/fa"
+import { FaUser } from "react-icons/fa";
 import { BsFillCreditCardFill } from "react-icons/bs";
-import checkIconMobile from "../assets/mobile/checkpoints.svg"
+import checkIconMobile from "../assets/mobile/checkpoints.svg";
+import { useSpring, animated } from "react-spring";
 
 const Content = () => {
-  return <section className="">
+  const customer = useSpring({ customers: 10245, from: { customers: 0 } });
+  const card = useSpring({ cards: 12045, from: { cards: 0 } });
+
+  return (
+    <section className="">
       <div className="ml-[70px] mr-[54px] text-bold shadow-md rounded-[24px] flex flex-col items-center bg-gradient-to-b from-white/40 to-transparent">
-          <div className="mt-16 gap-8 flex">
-              <FaUser size={"22px"} className="mt-[12px]"/>
-              <p className="text-26">
-                  10245
-                <p className="text-13 font-semibold mb-[36px]">
-                    Customer
-                </p>
-              </p>
+        <div className="mt-16 gap-8 flex">
+          <FaUser size={"22px"} className="mt-[12px]" />
+          <div className="text-26">
+            <animated.div>
+              {customer.customers.to((val) => Math.floor(val))}
+            </animated.div>
+            <p className="text-13 font-semibold mb-[36px]">Customer</p>
           </div>
-          <div className="gap-8 flex items-start mb-16">
-                <BsFillCreditCardFill size={"22px"} className="mt-[12px]"/>
-                <p className="text-26">
-                    12045
-                    <p className="text-13 font-semibold">Cards Issued</p>
-                </p>
+        </div>
+        <div className="gap-8 flex items-start mb-16">
+          <BsFillCreditCardFill size={"22px"} className="mt-[12px]" />
+          <div className="text-26">
+            <animated.div>
+              {card.cards.to((val) => Math.floor(val) /*round val*/)}
+            </animated.div>
+            <p className="text-13 font-semibold">Cards Issued</p>
           </div>
-      </div>  
-      <div className="text-13 flex flex-col w-[100%] mt-[90px] font-semibold">
-            <p className="check-content">
-                <img src={checkIconMobile} alt="" />
-                <p>Card reports sent to your phone every weeks</p>
-            </p>
-            <p className="check-content">
-                <img src={checkIconMobile} alt="" />
-                <p>No external fees</p>
-            </p>
-            <p className="check-content">
-                <img src={checkIconMobile} alt="" />
-                <p>Set spending limits and restriction</p>
-            </p>
+        </div>
       </div>
-  </section>;
+      <div className="text-13 flex flex-col w-[100%] mt-[90px] font-semibold">
+        <div className="check-content">
+          <img src={checkIconMobile} alt="" />
+          <p>Card reports sent to your phone every weeks</p>
+        </div>
+        <div className="check-content">
+          <img src={checkIconMobile} alt="" />
+          <p>No external fees</p>
+        </div>
+        <div className="check-content">
+          <img src={checkIconMobile} alt="" />
+          <p>Set spending limits and restriction</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Content;
