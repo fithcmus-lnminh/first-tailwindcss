99346529c6133e6dd144e851e965cd6e5441f305
Lê Nhật Minh <lenhatminh11a1@gmail.com>
2022-01-22 16:23:16 +0700

    custom colors and fontSize

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="h-screen px-4 py-8 bg-gradient-to-b from-white to-pink-500">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
