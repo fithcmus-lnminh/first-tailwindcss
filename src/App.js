28fed256a4257878b495c5d36f6863f3d80c01fe
Lê Nhật Minh <lenhatminh11a1@gmail.com>
2022-01-22 22:04:49 +0700

    nav, content, header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Content from "./components/Content";
+import Navbar from "./components/Navbar";
+import Slogan from "./components/Slogan";
 
 function App() {
   return (
-    <div className="h-screen px-4 py-8 bg-gradient-to-b from-white to-pink-500">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="font-sora overflow-y-auto overflow-x-hidden h-screen px-4 py-8 bg-gradient-to-b from-purple-900 to-purple-700 text-white">
+      <Navbar />
+      <Slogan />
+      <Content />
     </div>
   );
 }
---
+++ src/components/Content.jsx
@@ -0,0 +1,42 @@
+import {FaUser} from "react-icons/fa"
+import { BsFillCreditCardFill } from "react-icons/bs";
+import checkIconMobile from "../assets/mobile/checkpoints.svg"
+
+const Content = () => {
+  return <section className="">
+      <div className="ml-[70px] mr-[54px] text-bold shadow-md rounded-[24px] flex flex-col items-center bg-gradient-to-b from-white/40 to-transparent">
+          <div className="mt-16 gap-8 flex">
+              <FaUser size={"22px"} className="mt-[12px]"/>
+              <p className="text-26">
+                  10245
+                <p className="text-13 font-semibold mb-[36px]">
+                    Customer
+                </p>
+              </p>
+          </div>
+          <div className="gap-8 flex items-start mb-16">
+                <BsFillCreditCardFill size={"22px"} className="mt-[12px]"/>
+                <p className="text-26">
+                    12045
+                    <p className="text-13 font-semibold">Cards Issued</p>
+                </p>
+          </div>
+      </div>  
+      <div className="text-13 flex flex-col w-[100%] mt-[90px] font-semibold">
+            <p className="flex gap-8 ml-[70px] mr-[29px] items-center mb-[30px]">
+                <img src={checkIconMobile} alt="" />
+                <p>Card reports sent to your phone every weeks</p>
+            </p>
+            <p className="flex gap-8 ml-[70px] mr-[29px] items-center mb-[30px]">
+                <img src={checkIconMobile} alt="" />
+                <p>No external fees</p>
+            </p>
+            <p className="flex gap-8 ml-[70px] mr-[29px] items-center mb-[30px]">
+                <img src={checkIconMobile} alt="" />
+                <p>Set spending limits and restriction</p>
+            </p>
+      </div>
+  </section>;
+};
+
+export default Content;
